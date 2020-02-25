4b6c4d3c89b65a8b504ca604a5bd5e426665a729
omrobbie <putu.roby@yahoo.com>
2020-02-25 18:06:00 +0800

    setup signature pad

--- CPPT/Controller/InputVC.swift
+++ CPPT/Controller/InputVC.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import SignaturePad
 
 class InputVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var constraintBottom: NSLayoutConstraint!
+    @IBOutlet weak var signaturePad: SignaturePad!
+    @IBOutlet weak var btnSignatureStart: CustomUIButton!
 
     private var constraintBottomDefault: CGFloat = 0.0
 
@@ -40,5 +44,20 @@ class InputVC: UIViewController {
     func checkIfKeyboardShown() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @IBAction func btnSignatureStartTapped(_ sender: Any) {
+        let toggle = scrollView.isScrollEnabled
+        scrollView.isScrollEnabled = !toggle
+
+        if toggle {
+            btnSignatureStart.backgroundColor = #colorLiteral(red: 0.1806240082, green: 0.2743958831, blue: 0.4199774861, alpha: 1)
+        } else {
+            btnSignatureStart.backgroundColor = #colorLiteral(red: 0.3236097395, green: 0.4890208244, blue: 0.7480755448, alpha: 1)
+        }
+    }
+
+    @IBAction func btnSignatureClearTapped(_ sender: Any) {
+        signaturePad.clear()
     }
 }
