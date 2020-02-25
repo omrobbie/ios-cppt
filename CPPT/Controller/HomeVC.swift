21dafdfe2d88e52406df75cd2e480b11a205c16a
omrobbie <putu.roby@yahoo.com>
2020-02-25 14:37:34 +0800

    setup patient cell layout

--- CPPT/Controller/HomeVC.swift
+++ CPPT/Controller/HomeVC.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PatientCell") as? PatientCell else {return UITableViewCell()}
+
+        return cell
     }
 }
