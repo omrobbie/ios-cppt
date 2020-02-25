bf6e96729719b7cad7a00609dc23aa1af5cce357
omrobbie <putu.roby@yahoo.com>
2020-02-25 12:08:06 +0800

    setup login layout

---
+++ CPPT/Controller/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  CPPT
+//
+//  Created by omrobbie on 25/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var btnLogin: CustomUIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func setupUI() {
+        txtEmail.inputAccessoryView = btnLogin
+        txtPassword.inputAccessoryView = btnLogin
+    }
+
+    @IBAction func btnLoginTapped(_ sender: Any) {
+    }
+}
